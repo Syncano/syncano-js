f7f4cf00678302bcb75593012d5c66de9f0afc41
Daniel Kopka <daniel.kopka@syncano.com>
2016-01-02 21:54:45 +0100

    [LIB-383] cleanup

--- test/integration/instancesTest.js
+++ test/integration/instancesTest.js
@@ -94,12 +94,29 @@ describe('Instance', function() {
 
   describe('#please()', function() {
 
-    it('shoule be able to list instances', function() {
-      return connection.Instance.please().list();
+    it('shoule be able to list instances', function(done) {
+      connection.Instance.please().list().then((instances) => {
+        should(instances).be.an.Array();
+      }).catch((err) => {
+        throw err;
+      })
+      .finally(() => done());
     });
 
-    it('shoule be able to create an instance', function() {
-      return connection.Instance.please().create({name: instanceName});
+    it('shoule be able to create an instance', function(done) {
+      connection.Instance.please().create({name: instanceName}).then((instance) => {
+        should(instance).be.a.Object();
+        should(instance).have.property('name').which.is.String().equal(instanceName);
+        should(instance).have.property('description').which.is.String();
+        should(instance).have.property('created_at').which.is.String();
+        should(instance).have.property('updated_at').which.is.String();
+        should(instance).have.property('links').which.is.Object();
+        should(instance).have.property('owner').which.is.Object();
+        should(instance).have.property('metadata').which.is.Object();
+      }).catch((err) => {
+        throw err;
+      })
+      .finally(() => done());
     });
 
     it('shoule be able to get an instance', function(done) {
@@ -107,21 +124,29 @@ describe('Instance', function() {
         .then((instance) => {
           should(instance).be.an.Object();
           should(instance).have.property('name').which.is.String().equal(instanceName);
-
-          connection.Instance.please().get({name: instanceName})
-            .then((newInstance) => {
-              should(newInstance).be.an.Object();
-              should(newInstance).have.property('name').which.is.String().equal(instanceName);
-              should(newInstance).have.property('name').which.is.String().equal(instance.name);
-
-              done();
-            }).catch((err) => {
-              throw err;
-            });
-
-        }).catch((err) => {
+          return instance;
+        })
+        .then(() => {
+          return connection
+            .Instance
+            .please()
+            .get({name: instanceName})
+            .request();
+        })
+        .then((instance) => {
+          should(instance).be.an.Object();
+          should(instance).have.property('name').which.is.String().equal(instanceName);
+          should(instance).have.property('description').which.is.String();
+          should(instance).have.property('created_at').which.is.String();
+          should(instance).have.property('updated_at').which.is.String();
+          should(instance).have.property('links').which.is.Object();
+          should(instance).have.property('owner').which.is.Object();
+          should(instance).have.property('metadata').which.is.Object();
+        })
+        .catch((err) => {
           throw err;
-        });
+        })
+        .finally(() => done());
     });
 
     it('shoule be able to delete an instance', function(done) {
@@ -129,16 +154,63 @@ describe('Instance', function() {
         .then((instance) => {
           should(instance).be.an.Object();
           should(instance).have.property('name').which.is.String().equal(instanceName);
-
-          connection.Instance.please().delete({name: instanceName})
-            .then(() => {
-              done();
-            }).catch((err) => {
-              throw err;
-            });
-        }).catch((err) => {
+          return instance;
+        })
+        .then((instance) => {
+          return connection
+            .Instance
+            .please()
+            .delete({name: instanceName})
+            .request();
+        })
+        .catch((err) => {
           throw err;
-        });
+        })
+        .finally(() => done());
+    });
+
+    it('shoule be able to get or create instance (CREATE)', function() {
+
+    });
+
+    it('shoule be able to get or create instance (GET)', function() {
+
+    });
+
+    it('shoule be able to update an instance', function() {
+
+    });
+
+    it('shoule be able to update or create instance (UPDATE)', function() {
+
+    });
+
+    it('shoule be able to update or create instance (CREATE)', function() {
+
+    });
+
+    it('shoule be able to get first instance', function() {
+
+    });
+
+    it('shoule be able to change page size', function() {
+
+    });
+
+    it('shoule be able to change ordering', function() {
+
+    });
+
+    it('shoule be able to get raw data', function(done) {
+      connection.Instance.please().list().raw().then((response) => {
+        should(response).be.a.Object();
+        should(response).have.property('objects').which.is.Array();
+        should(response).have.property('next').which.is.null();
+        should(response).have.property('prev').which.is.null();
+      }).catch((err) => {
+        throw err;
+      })
+      .finally(() => done());
     });
 
   });
