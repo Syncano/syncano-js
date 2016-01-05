aed88c9826109dfbfda00967cb88165c27f30b5e
Daniel Kopka <daniel.kopka@syncano.com>
2016-01-05 09:58:26 +0100

    [LIB-397] errors

--- src/errors.js
+++ src/errors.js
@@ -1,21 +1,39 @@
 export class SyncanoError extends Error {
-  constructor(message) {
+  constructor(message = '') {
     super(message);
-    this.message = message;
-    this.name = 'SyncanoError';
+
+    // extending Error is weird and does not propagate `message`
+    Object.defineProperty(this, 'message', {
+      enumerable : false,
+      value : message
+    });
+
+    Object.defineProperty(this, 'name', {
+      enumerable : false,
+      value : name || this.constructor.name,
+    });
+
+    if (Error.hasOwnProperty('captureStackTrace')) {
+      Error.captureStackTrace(this, this.constructor);
+      return;
+    }
+
+    Object.defineProperty(this, 'stack', {
+      enumerable : false,
+      value : (new Error(message)).stack,
+    });
   }
 }
 
 export class ValidationError extends SyncanoError {
 
   constructor(errors = {}) {
-    let message = _.reduce(errors, (result, value, key) => {
+    const message = _.reduce(errors, (result, value, key) => {
       result += `${key} - ${value.join()} `;
       return result;
     }, '');
 
     super(message);
-    this.name = 'ValidationError';
     this.errors = errors;
   }
 
