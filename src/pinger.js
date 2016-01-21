6bd0cf6eb0b94e9fcb8930be304c2296588b66f5
Jakub Bilko <kozulowski@gmail.com>
2016-01-21 11:01:16 +0100

    [LIB-300] Pinger

---
+++ src/pinger.js
@@ -0,0 +1,41 @@
+import stampit from 'stampit';
+import {EventEmittable} from './utils';
+import Request from './request';
+
+const Pinger = stampit()
+  .compose(Request, EventEmittable)
+  .props({
+    timeout: 5000,
+    interval: null
+  })
+  .methods({
+
+    request() {
+      const path = this._config.baseUrl;
+
+      return new Promise((resolve, reject) => {
+        this.makeRequest('GET', path, {}, (err, res) => {
+          if (err || !res.ok) {
+            return reject(err, res);
+          }
+          resolve(res.body, res);
+        });
+      })
+    },
+
+    startMonitoring() {
+      this.interval = setInterval(() => this.ping(), this.timeout);
+    },
+
+    ping() {
+      this.request()
+        .then(() => this.emit('connect'))
+        .catch((error) => this.emit('disconnect', error));
+    },
+
+    stopMonitoring() {
+      clearInterval(this.interval);
+    }
+  });
+
+export default Pinger;
