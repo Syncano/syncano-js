e1f039e4baf8f9db3cf4de4fddc54324d6de20e9
jakubbilko <kozulowski@gmail.com>
2016-01-21 20:02:29 +0100

    [LIB-437] Template model-specific instance methods

--- src/models/template.js
+++ src/models/template.js
@@ -7,7 +7,7 @@ const TemplateMeta = Meta({
   endpoints: {
     'detail': {
       'methods': ['delete', 'patch', 'put', 'get'],
-      'path': '/v1/instances/{instanceName}/snippets/templates/{name}'
+      'path': '/v1/instances/{instanceName}/snippets/templates/{name}/'
     },
     'list': {
       'methods': ['post', 'get'],
@@ -25,6 +25,12 @@ const TemplateMeta = Meta({
 });
 
 const TemplateConstraints = {
+  name: {
+    presence: true,
+    length: {
+      minimum: 5
+    }
+  },
   instanceName: {
     presence: true,
     length: {
@@ -36,6 +42,34 @@ const TemplateConstraints = {
 const Template = stampit()
   .compose(Model)
   .setMeta(TemplateMeta)
+  .methods({
+
+    request(path, options) {
+      return new Promise((resolve, reject) => {
+        this.makeRequest('POST', path, options, (err, res) => {
+          if (err || !res.ok) {
+            return reject(err, res);
+          }
+          resolve(res.body, res);
+        });
+      });
+    },
+
+    rename(options = {}) {
+      const meta = this.getMeta();
+      const path = meta.resolveEndpointPath('rename', this);
+
+      return this.request(path, options);
+    },
+
+    render(options = {}) {
+      const meta = this.getMeta();
+      const path = meta.resolveEndpointPath('render', this);
+
+      return this.request(path, options);
+    }
+
+  })
   .setConstraints(TemplateConstraints);
 
 export default Template;
