66408d87e57923cc35a2ec5c33b90ff2671af358
Adam Wardecki <adam.wardecki@gmail.com>
2016-02-11 14:21:06 +0100

    [LIB-393] Added fetchData() method to DataView model

--- src/models/dataview.js
+++ src/models/dataview.js
@@ -1,5 +1,34 @@
 import stampit from 'stampit';
+import _ from 'lodash';
 import {Meta, Model} from './base';
+import QuerySet from '../querySet';
+
+
+const DataViewQerySet = stampit().compose(QuerySet).methods({
+
+  /**
+  * Fetches Data Objects matching the Data View properties.
+  * @memberOf DataViewQerySet
+  * @instance
+
+  * @param {Object} properties lookup properties used for path resolving
+  * @returns {DataViewQerySet}
+
+  * @example {@lang javascript}
+  * DataView.please().fetchData({name: 'dataViewName', instanceName: 'test-one'}).then(function(dataObjects) {});
+
+  */
+  fetchData(properties = {}) {
+    this.properties = _.assign({}, this.properties, properties);
+
+    this.method = 'GET';
+    this.endpoint = 'get';
+    this._serialize = false;
+
+    return this;
+  }
+
+});
 
 const DataViewMeta = Meta({
   name: 'dataview',
@@ -30,6 +59,35 @@ const DataViewMeta = Meta({
 
 const DataView = stampit()
   .compose(Model)
-  .setMeta(DataViewMeta);
+  .setMeta(DataViewMeta)
+  .setQuerySet(DataViewQerySet)
+  .methods({
+
+    /**
+    * Fetches Data Objects matching the Data View properties.
+    * @memberOf DataView
+    * @instance
+
+    * @param {Object}
+    * @returns {Promise}
+
+    * @example {@lang javascript}
+    * DataView.please().fetchData({name: 'dataViewName', instanceName: 'test-one'}).then(function(dataObjects) {});
+    */
+    fetchData() {
+      const meta = this.getMeta();
+      const path = meta.resolveEndpointPath('get', this);
+
+      return new Promise((resolve, reject) => {
+        this.makeRequest('GET', path, (err, res) => {
+          if (err || !res.ok) {
+            return reject(err, res);
+          }
+          resolve(res.body, res);
+        });
+      });
+    }
+
+  });
 
 export default DataView;
