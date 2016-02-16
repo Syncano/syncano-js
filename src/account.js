e931ace58c43f555eb3cda4ad9329382766172c3
Daniel Kopka <daniel.kopka@syncano.com>
2016-02-16 16:03:56 +0100

    [LIB-508] As a user I can login via account (admin) login and password

--- src/account.js
+++ src/account.js
@@ -2,19 +2,84 @@ import stampit from 'stampit';
 import Request from './request';
 
 
+/**
+ * Wrapper around account endpoint. Meant to be used for {@link Syncano} instance.
+
+ * @constructor
+ * @type {Account}
+
+ * @example {@lang javascript}
+ * const {Account} = Syncano();
+ * Account.login({email: '', password: ''}).then((user) => );
+ */
 const Account = stampit().compose(Request)
   .props({
     _account: {
-      registerPath: '/v1/account/register/'
+      registerPath: '/v1/account/register/',
+      loginPath: '/v1/account/auth/'
     }
   })
   .methods({
-    register(user = {}) {
-      const path = this._account.registerPath;
-      const options = {payload: user};
 
-      return this.makeRequest('POST', path, options);
+    /**
+    * A convenience method for creating a new account.
+
+    * @memberOf Account
+    * @instance
+
+    * @param {Object} payload
+    * @param {String} payload.email
+    * @param {String} payload.password
+    * @returns {Promise}
+
+    */
+    register(payload = {}) {
+      const path = this._account.registerPath;
+      return this.makeRequest('POST', path, {payload});
+    },
+
+    /**
+    * A convenience method for authenticating with email and password.
+
+    * @memberOf Account
+    * @instance
+
+    * @param {Object} credentials
+    * @param {String} credentials.email
+    * @param {String} credentials.password
+    * @param {Boolean} [setAccountKey = true]
+    * @returns {Promise}
+
+    */
+    login(payload = {}, setAccountKey = true) {
+      const config = this.getConfig();
+      const path = this._account.loginPath;
+
+      return this.makeRequest('POST', path, {payload}).then((user) => {
+        if (setAccountKey === true) {
+          config.setAccountKey(user.account_key);
+        }
+        return user;
+      });
+    },
+
+    /**
+    * A convenience method for updating your account details.
+
+    * @memberOf Account
+    * @instance
+
+    * @param {Object} payload
+    * @param {String} payload.first_name
+    * @param {String} payload.last_name
+    * @returns {Promise}
+
+    */
+    update(payload = {}) {
+      const path = this._account.loginPath;
+      return this.makeRequest('POST', path, {payload});
     }
+
   });
 
 
