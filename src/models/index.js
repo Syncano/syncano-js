549e53d4ce3cf70e25bad8578174611e1e437af1
jakubbilko <kozulowski@gmail.com>
2016-05-16 09:17:55 +0200

    [LIB-724] Semicolons

--- src/models/index.js
+++ src/models/index.js
@@ -8,7 +8,7 @@ import Admin from './admin';
 import ApiKey from './apikey';
 import InstanceInvitation from './instanceinvitation';
 import Invitation from './invitation';
-import Invoice from './invoice'
+import Invoice from './invoice';
 import Script from './script';
 import Schedule from './schedule';
 import Trigger from './trigger';
@@ -24,7 +24,7 @@ import APNSDevice from './apnsdevice';
 import APNSConfig from './apnsconfig';
 import GCMMessage from './gcmmessage';
 import APNSMessage from './apnsmessage';
-import Template from './template'
+import Template from './template';
 
 export default {
   Instance,
