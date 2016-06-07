ce1ee9aae0208861d9b653b37a233c004842a171
jakubbilko <kozulowski@gmail.com>
2016-06-07 09:29:23 +0200

    [LIB-708] Remove unnecessary test case

--- test/integration/dataobjectsTest.js
+++ test/integration/dataobjectsTest.js
@@ -438,39 +438,6 @@ describe('Dataobject', function() {
         });
     });
 
-    xit('should be able to add unique relations', function() {
-      let authorIds = null;
-
-      return Model.please().bulkCreate([Model(philipKDick), Model(charlesBukowski)])
-        .then(cleaner.mark)
-        .then((authors) => {
-          should(authors).be.an.Array().with.length(2);
-
-          authorIds = _.map(authors, (x) => x.id)
-
-          return Model.please().create({instanceName, className: 'books', title: 'Ubik', authors: [authorIds[0]]});
-        })
-        .then(cleaner.mark)
-        .then((book) => {
-          should(book).be.an.Object();
-          should(book).have.property('instanceName').which.is.String().equal(instanceName);
-          should(book).have.property('className').which.is.String().equal('books');
-          should(book).have.property('title').which.is.String().equal('Ubik');
-          should(book).have.property('authors').which.is.Object();
-          should(book.authors).have.property('target').which.is.String().equal('authors');
-          should(book.authors).have.property('value').which.is.Array().with.length(1);
-          should(book.authors.value[0]).be.a.Number().equal(authorIds[0]);
-
-          return Model.please().addUnique({id: book.id, instanceName, className: 'books'}, { authors: authorIds });
-        })
-        .then((book) => {
-          should(book.authors).have.property('target').which.is.String().equal('authors');
-          should(book.authors).have.property('value').which.is.Array().with.length(2);
-          should(book.authors.value[0]).be.a.Number().equal(authorIds[0]);
-          should(book.authors.value[1]).be.a.Number().equal(authorIds[1]);
-        })
-    });
-
     it('should be able to filter objects by relation using #is()', function() {
       let authorIds = null;
       const books = [
