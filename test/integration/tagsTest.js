87722234df9ec6b4994a7fd09d8a77415b520e73
jakubbilko <kozulowski@gmail.com>
2016-07-19 21:29:54 +0200

    [LIB-762] Tag model tests instance name change

--- test/integration/tagsTest.js
+++ test/integration/tagsTest.js
@@ -8,7 +8,7 @@ describe('Tag', function() {
   let connection = null;
   let Model = null;
   let Instance = null;
-  const instanceName = suffix.get('Usage');
+  const instanceName = suffix.get('Tag');
 
   before(function() {
     connection = Syncano(credentials.getCredentials());
